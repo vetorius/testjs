fe9448da3efe3ec9f4b087502adeea9bf3dfbd2f
Víctor Manuel Sánchez <vetorius@gmail.com>
2021-03-01 20:27:12 +0100

    arreglar error parseado 1

--- menucurso.js
+++ menucurso.js
@@ -73,6 +73,7 @@ function rescatarDatosCurso(){
           activities: []
         }
         submatrix.forEach(function(actividad){
+          console.log(actividad);
           const theActivity = {
             name: actividad[2],
             link: actividad[3],
